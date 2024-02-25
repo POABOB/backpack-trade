46d8a0d2d06dd602061bbeac9ba66c4cfb47a8a7
POABOB <zxc752166@gmail.com>
2024-02-25 14:18:26 +0000

    fix: dix some bug.

--- trade.js
+++ trade.js
@@ -1,82 +1,56 @@
 // 交易模式
 // 0: 以市價方式進行買賣，沒有停止機制，若需要停止請重整網頁
-// 1：以限價方式進行買賣，並且每次買賣限制10U
-const tradeType = 1;
+const tradeType = 0;
 
 // 交易停止規則
 // 0：無限制
 // 1：設定一個 USDC 的閥值，若餘額低於該閥值則停止
 // 2：以交易次數進行控制
-const tradeVolume = 2;
+const tradeVolume = 0;
 
 // 設定 USDC 剩餘某數量後停止
-const stopUsdc = 0;
+const stopUsdc = 10;
 
 // 設定停止的交易次數
 const stopTradeAmout = 100;
-
-// 每次買賣 10U
-const limitOrderAmout = 10;
 
 
 // 以下程式碼請勿修改
 let timer;
 let counter = 1;
 // 自動交易
-const trade = async () => {
-    let amountInput = document.getElementsByClassName("h-12 rounded-lg border-2 border-solid border-baseBorderLight bg-[var(--background)] pr-12 text-right text-2xl leading-9 text-[$text] placeholder-[var(--text2)] ring-0 transition focus:border-accentBlue focus:ring-0");
+const tradeMarket = async () => {
+    // 修改掛單類型
+    await changeOrderType()
     
-    if (amountInput.length == 2) {
-        // 當前是使用限價，改回市價
-        document.getElementsByClassName("flex flex-col cursor-pointer justify-center px-3 py-2")[9].click()
-        await new Promise(resolve => setTimeout(resolve, 500)); 
-    }
-    
-    let isLastest = false;
-    if (tradeType === 1) {
-        const balanceArray = document.getElementsByClassName('_ls-167744059')[5].textContent.split(' ');
-        const balance = balanceArray[0];
-        if (parseFloat(balance) < stopUsdc) {
-            console.log(`餘額已低於${stopUsdc} usdc，已停止`)
-            isLastest = true;
-        }
-    } else if (tradeType === 2) {
-        if (counter === stopTradeAmout) {
-            console.log(`交易次數已達到${stopTradeAmout}次，已停止`)
-            isLastest = true;
-        }
-    } else if (tradeType === 3) {
-
-    }
+    // 確認交易量是否達標
+    const isLastest = await checkVolume();
 
 
     // 點擊 MAX
     document.getElementsByClassName('_bg-901062054')[3].click();
     await new Promise(resolve => setTimeout(resolve, 100));
 
-    if (tradeType === 3) {
-        if (amountInput[1].value > 10) {
-            amountInput[1].defaultValue = 10;
-            amountInput[1].value = 10;
-            doEvent(amountInput[1], "input");
-        }
-    }
-
-
-
+    // 買入 SOL
     document.getElementsByClassName('bg-greenPrimaryButtonBackground')[0].click()
-    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    await new Promise(resolve => setTimeout(resolve, 750)); 
 
+    
     if (isLastest) {
-        stopTrade();
+        await stopTrade();
+        return;
     }
 
+    // 切換至 Sell
     document.getElementsByClassName('border-b-baseBorderMed')[0].click()
     await new Promise(resolve => setTimeout(resolve, 100)); 
+    // 點擊 MAX
     document.getElementsByClassName('_bg-901062054')[3].click();
     await new Promise(resolve => setTimeout(resolve, 100)); 
+    // 賣出 SOL
     document.getElementsByClassName('bg-redPrimaryButtonBackground')[0].click()
-    await new Promise(resolve => setTimeout(resolve, 1000)); 
+    await new Promise(resolve => setTimeout(resolve, 750)); 
+    // 切換至 Buy
     document.getElementsByClassName('border-b-baseBorderMed')[0].click()
     counter++;
 }
@@ -86,10 +60,38 @@ const stopTrade = () => {
     clearInterval(timer);
 }
 
-const doEvent = (obj, event) => {
-    var event = new Event( event, {target: obj, bubbles: true} );
-    return obj ? obj.dispatchEvent(event) : false;
+// 改變掛單類型
+const changeOrderType = async () => {
+    if (tradeType == 0) {
+        // 市價
+        document.getElementsByClassName("flex flex-col cursor-pointer justify-center px-3 py-2")[9].click()
+    } else {
+        // 限價
+        document.getElementsByClassName("flex flex-col cursor-pointer justify-center px-3 py-2")[8].click()
+    }
+    await new Promise(resolve => setTimeout(resolve, 500)); 
+}
+
+// 確認交易次數是否達標
+const checkVolume = async () => {
+    const isLastest = false;
+    const balanceArray = document.getElementsByClassName('_ls-167744059')[5].textContent.split(' ');
+    const balance = balanceArray[0];
+
+    if (tradeVolume === 1 && parseFloat(balance) < stopUsdc) {
+        console.log(`餘額已低於${stopUsdc} usdc，已停止`)
+        isLastest = true;
+    } else if (tradeVolume === 2 && counter >= stopTradeAmout) {
+        console.log(`交易次數已達到${stopTradeAmout}次，已停止`)
+        isLastest = true;
+    }
+
+    return isLastest;
 }
 
 // 3s 循环一次，想停止，刷新页面即可
-timer = setInterval(trade, 5000)
+if (tradeType === 1) {
+    timer = setInterval(tradeLimit, 5000);
+} else {
+    timer = setInterval(tradeMarket, 5000);
+}
